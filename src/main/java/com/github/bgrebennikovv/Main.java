e44bb1f992f434f2d605987244b4ce86a2f83df6
Boris <bgrebennikovv@gmail.com>
2024-12-23 23:33:00 +0400

    print in console

--- src/main/java/com/github/bgrebennikovv/Main.java
+++ src/main/java/com/github/bgrebennikovv/Main.java
@@ -5,13 +5,14 @@ public class Main {
 
         // Task 1
         int[] numbersArray = new int[]{1, 2, 3};
+        System.out.println(numbersArray);
 
         // Task 2
         double[] doubleArray = new double[3];
         doubleArray[0] = 1.57;
         doubleArray[1] = 7.654;
         doubleArray[2] = 9.986;
-
+        System.out.println(doubleArray);
 
 
     }
