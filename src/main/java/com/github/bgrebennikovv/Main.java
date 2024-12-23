aaf702e100e68421711ed920c864c4cb193e8f61
Boris <bgrebennikovv@gmail.com>
2024-12-23 23:56:26 +0400

    task 2

--- src/main/java/com/github/bgrebennikovv/Main.java
+++ src/main/java/com/github/bgrebennikovv/Main.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
+
     public static void main(String[] args) {
 
         // Task 1
@@ -24,6 +25,42 @@ public class Main {
             byteArray[i] = (byte) random.nextInt(128);
         }
         System.out.println(Arrays.toString(byteArray));
+
+        // Task 2
+        System.out.println("\nTask");
+
+        StringBuilder numbersArrayString = new StringBuilder();
+        for (int i = 0; i < numbersArray.length; i++) {
+            String separator = ", ";
+            if (i == numbersArray.length-1) {
+                separator = "";
+            }
+            numbersArrayString.append(String.format("%d%s", numbersArray[i], separator));
+        }
+
+        System.out.println(numbersArrayString);
+
+        StringBuilder doubleArrayString = new StringBuilder();
+        for (int i = 0; i < doubleArray.length; i++) {
+            String separator = ", ";
+            if (i == doubleArray.length-1) {
+                separator = "";
+            }
+            doubleArrayString.append(String.format("%s%s", doubleArray[i], separator));
+        }
+
+        System.out.println(doubleArrayString);
+
+        StringBuilder byteArrayString = new StringBuilder();
+        for (int i = 0; i < byteArray.length; i++) {
+            String separator = ", ";
+            if (i == byteArray.length-1) {
+                separator = "";
+            }
+            byteArrayString.append(String.format("%s%s", byteArray[i], separator));
+        }
+
+        System.out.println(byteArrayString);
 
 
     }
