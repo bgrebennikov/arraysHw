7f31edca8c695bddbb43f6b769df0b19dbca98f0
Boris <bgrebennikovv@gmail.com>
2024-12-23 23:32:34 +0400

    task 2

--- src/main/java/com/github/bgrebennikovv/Main.java
+++ src/main/java/com/github/bgrebennikovv/Main.java
@@ -6,6 +6,13 @@ public class Main {
         // Task 1
         int[] numbersArray = new int[]{1, 2, 3};
 
+        // Task 2
+        double[] doubleArray = new double[3];
+        doubleArray[0] = 1.57;
+        doubleArray[1] = 7.654;
+        doubleArray[2] = 9.986;
+
+
 
     }
 }
