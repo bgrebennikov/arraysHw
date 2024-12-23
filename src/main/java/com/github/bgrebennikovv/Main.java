8283be6a3893b654d4d454cc14a566a5caf9b3b7
Boris <bgrebennikovv@gmail.com>
2024-12-23 23:30:22 +0400

    task 1

--- src/main/java/com/github/bgrebennikovv/Main.java
+++ src/main/java/com/github/bgrebennikovv/Main.java
@@ -3,5 +3,9 @@ package com.github.bgrebennikovv;
 public class Main {
     public static void main(String[] args) {
 
+        // Task 1
+        int[] numbersArray = new int[]{1, 2, 3};
+
+
     }
 }
