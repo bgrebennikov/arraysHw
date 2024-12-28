f7ce7af3e802ae3461a8a8882797fcd8490d8285
Boris <bgrebennikovv@gmail.com>
2024-12-28 15:45:37 +0400

    task 1

--- src/main/java/com/github/bgrebennikovv/Main.java
+++ src/main/java/com/github/bgrebennikovv/Main.java
@@ -3,5 +3,18 @@ package com.github.bgrebennikovv;
 public class Main {
     public static void main(String[] args) {
 
+        // Task 1
+        System.out.println("Task 1");
+        int[] monthlyExpenses = new int[]{11_000, 560, 2_000, 3500, 50};
+
+        int sumOfExpenses = 0;
+        for (int i : monthlyExpenses) {
+            sumOfExpenses += i;
+        }
+
+        System.out.printf("""
+                Сумма трат за месяц составила %s рублей
+                """, sumOfExpenses);
+
     }
 }
